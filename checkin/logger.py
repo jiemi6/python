59d6a3b9b13f4fbb509edc046af5cea989e993ec
mijj <minkey>
2019-12-26 13:47:01 +0800

    修改之后的

---
+++ checkin/logger.py
@@ -0,0 +1,26 @@
+﻿#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+import logging
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+#logging.basicConfig(filename = os.path.join(os.getcwd(), 'checkin.log'), level = logging.DEBUG)
+logging.basicConfig(level = logging.DEBUG,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+logger.addHandler(console)
+
+handler = logging.FileHandler("checkin.log")
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+logger.warn("22222222222222")
---
+++ checkin/main.py
@@ -0,0 +1,50 @@
+﻿#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+import logging
+import schedule
+import time
+import traceback
+import checkin_sokindle
+#import checkin_smzdm
+
+import os
+import logging
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+#logging.basicConfig(filename = os.path.join(os.getcwd(), 'checkin.log'), level = logging.DEBUG)
+logging.basicConfig(level = logging.DEBUG,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+
+logging.warn("-----开始运行了")
+
+def job():
+	logging.warn("------inf")
+	logging.debug("----debug")
+#  try:
+#  	checkin_smzdm.checkin()
+#  except BaseException,e:
+#  	logging.debug("checkin_smzdm报错了："+str(e))
+#  	traceback.print_exc()
+
+	try:
+		checkin_sokindle.checkin()
+	except BaseException,e:
+		logging.debug("checkin_sokindle报错了："+str(e))
+		traceback.print_exc()
+
+#checkin_smzdm.checkin()
+checkin_sokindle.checkin()
+
+#schedule.every(10).seconds.do(job)
+#schedule.every().day.at("08:30").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(10000)
